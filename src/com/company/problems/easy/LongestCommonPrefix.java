e5386d16f641da2f8cf2cfccee37bff057213cc4
tsiupryk <tsupryk@gmail.com>
2022-08-06 13:02:11 +0300

    Added LongestCommonPrefix task solution

---
+++ src/com/company/problems/easy/LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+package com.company.problems.easy;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "". <br>
+ * Example 1: <br>
+ * Input: strs = ["flower","flow","flight"] Output: "fl"
+ * <a href="https://leetcode.com/problems/longest-common-prefix/">problem</a>
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        LongestCommonPrefix solution = new LongestCommonPrefix();
+
+        assert solution.longestCommonPrefix(new String[]{"flower","flow","flight"}).equals("fl");
+        assert solution.longestCommonPrefix(new String[]{"dog","racecar","car"}).equals("");
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while (allMatch(strs, index)) {
+            sb.append(strs[0].charAt(index++));
+        }
+        return sb.toString();
+    }
+
+    private boolean allMatch(String[] strs, int index) {
+        if (index >= strs[0].length()) {
+            return false;
+        }
+        char currChar = strs[0].charAt(index);
+        for (String str : strs) {
+            if (index >= str.length() || str.charAt(index) != currChar) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
