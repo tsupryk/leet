3e8fab0019d5860f1bb9a1b7234cd65052ef5677
tsiupryk <tsupryk@gmail.com>
2022-08-08 15:45:17 +0300

    MergeTwoSortedLists complete (v2, simpler)

--- src/com/company/problems/easy/MergeTwoSortedLists.java
+++ src/com/company/problems/easy/MergeTwoSortedLists.java
@@ -5,13 +5,12 @@ import com.company.problems.Test;
 public class MergeTwoSortedLists {
 
     public static void main(String[] args) {
-        var root1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        var root2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-
         MergeTwoSortedLists solution = new MergeTwoSortedLists();
 
+        // case 1
+        var root1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        var root2 = new ListNode(1, new ListNode(3, new ListNode(4)));
         ListNode result = solution.mergeTwoLists(root1, root2);
-
         Test.isTrue(
                 result.val == 1
                 && result.next.val == 1
@@ -19,66 +18,65 @@ public class MergeTwoSortedLists {
                 && result.next.next.next.val == 3
                 && result.next.next.next.next.val == 4
                 && result.next.next.next.next.next.val == 4
+                && result.next.next.next.next.next.next == null
         );
-        Test.equals(result.next.next.next.next.next.next, null);
 
-        //
-//        var root3 = new ListNode(1);
-//        var root4 = new ListNode(1, new ListNode(3, new ListNode(4)));
-//        ListNode result2 = solution.mergeTwoLists(root3, root4);
-//        ListNode result3 = solution.mergeTwoLists(null, root4);
-//        Test.isTrue(true);
+        // case 2
+        var root3 = new ListNode(1);
+        var root4 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode result2 = solution.mergeTwoLists(root3, root4);
+        Test.isTrue(
+                result2.val == 1
+                        && result2.next.val == 1
+                        && result2.next.next.val == 3
+                        && result2.next.next.next.val == 4
+        );
+        Test.equals(result2.next.next.next.next, null);
+
+        // case 4
+        ListNode result3 = solution.mergeTwoLists(null, root4);
+        Test.isTrue(
+                result3.val == 1
+                        && result3.next.val == 3
+                        && result3.next.next.val == 4
+        );
+        Test.equals(result3.next.next.next, null);
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) { return list2; }
         if (list2 == null) { return list1; }
 
-        Step step = new Step(list1, list2);
-        ListNode root = getSmaller(step);
-        ListNode currentNode = root;
-        ListNode node = currentNode;
-        while (node != null) {
-            currentNode.next = node;
-            currentNode = node;
-            node = getSmaller(step);
-        }
-        currentNode.next = null;
-        return root;
-    }
+        ListNode root = null;
+        ListNode currentNode = null;
 
-    private ListNode getSmaller(Step step) {
-        ListNode result;
-        if (step.list1 == null) {
-            result = step.list2;
-            if (step.list2 != null) {
-                step.list2 = step.list2.next;
-            }
-            return result;
-        }
-        if (step.list2 == null) {
-            result = step.list1;
-            step.list1 = step.list1.next;
-            return result;
-        }
-        if (step.list1.val < step.list2.val) {
-            result = step.list1;
-            step.list1 = step.list1.next;
+        if (list1.val > list2.val) {
+            root = list2;
+            list2 = list2.next;
         } else {
-            result = step.list2;
-            step.list2 = step.list2.next;
+            root = list1;
+            list1 = list1.next;
         }
-        return result;
-    }
 
-    static class Step {
-        ListNode list1;
-        ListNode list2;
-
-        public Step(ListNode list1, ListNode list2) {
-            this.list1 = list1;
-            this.list2 = list2;
+        currentNode = root;
+        while (currentNode != null) {
+            if (list1 == null) {
+                currentNode.next = list2;
+                return root;
+            } else if (list2 == null) {
+                currentNode.next = list1;
+                return root;
+            } else if (list1.val > list2.val) {
+                currentNode.next = list2;
+                list2 = list2.next;
+            } else {
+                currentNode.next = list1;
+                list1 = list1.next;
+            }
+            currentNode = currentNode.next;
         }
+        return root;
     }
 
     static class ListNode {
